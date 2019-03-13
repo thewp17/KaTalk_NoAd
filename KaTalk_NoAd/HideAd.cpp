cad70b2af1bd63ec9ebfb1420add7e68be634d7c
Deokhyun-Yun <dhyun@saminis.com>
2019-03-13 11:33:33 +0900

    광고 위치 수정안되는 문제 수정

--- KaTalk_NoAd/HideAd.cpp
+++ KaTalk_NoAd/HideAd.cpp
@@ -5,8 +5,8 @@ BOOL CALLBACK EnumWindowProc(HWND hwnd, LPARAM lParam)
 	TCHAR classname[100] = { 0 };
 	TCHAR titlename[100] = { 0 };
 
-	GetClassName(hwnd, (LPTSTR)&classname, 100);
-	GetWindowText(hwnd, (LPTSTR)&titlename, 256);
+	GetClassName(hwnd, (LPTSTR)& classname, 100);
+	GetWindowText(hwnd, (LPTSTR)& titlename, 256);
 
 	return true;
 }
@@ -23,12 +23,22 @@ HideAd::HideAd()
 	{
 		RECT rect;
 		GetWindowRect(hwnd, &rect);
-		HWND friendList = FindWindowEx(hwnd, nullptr, "#32770", nullptr);
+		
 		HWND childAD = FindWindowEx(hwnd, nullptr, "EVA_Window", nullptr);
 		ShowWindow(childAD, SW_HIDE);
-
 		SetWindowPos(childAD, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE);
-		SetWindowPos(friendList, HWND_BOTTOM, 0, 0, (rect.right - rect.left), (rect.bottom - rect.top - 36), SWP_NOMOVE);
+		
+		HWND mainview = FindWindowEx(hwnd, nullptr, "EVA_ChildWindow", nullptr);
+		while (mainview != nullptr)
+		{
+			TCHAR name[15] = "";
+			GetWindowText(mainview, name, 15);
+			if (0 == _tcscmp(name, "OnlineMainView"))
+			{
+				MoveWindow(mainview, 0, 30, (rect.right - rect.left), (rect.bottom - rect.top - 30), TRUE);
+				break;
+			}
+		}
 	}
 
 	// 랜덤으로 팝업되는 오른쪽 하단의 투명 광고
@@ -62,9 +72,10 @@ bool HideAd::isExecution()
 		sInfo.nShow = SW_SHOW;
 		sInfo.lpFile = "C:\\Program Files (x86)\\Kakao\\KakaoTalk\\KakaoTalk.exe";
 		ShellExecuteEx(&sInfo);
-		Sleep(10000);
+		
+		MessageBox(nullptr, "카카오톡이 곧 실행됩니다. 실행 후 다시 시작하세요.", "확인", MB_OK);
 	}
-	
+
 	return false;
 }
 
